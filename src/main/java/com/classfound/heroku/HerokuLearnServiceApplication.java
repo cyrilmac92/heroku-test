72ea2aa622911c010c8293d51c4037be31bf6a86
cyril mac philip <cyrilmac92@yahoo.com>
2016-05-21 20:55:55 +0530

    added Application.java

---
+++ src/main/java/com/classfound/heroku/HerokuLearnServiceApplication.java
@@ -0,0 +1,12 @@
+package com.classfound.heroku;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HerokuLearnServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HerokuLearnServiceApplication.class, args);
+	}
+}
